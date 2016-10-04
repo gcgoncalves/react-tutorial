74ab0988281e5f140596241caf7be6eedc5e9ea1
Gabriel Costa <gcgoncalves88@gmail.com>
2016-10-03 23:10:01 -0300

    Create a ballot from a state

--- __tests__/core.js
+++ __tests__/core.js
@@ -11,3 +11,17 @@ test('Set entries', function () {
     entries: immutable.List.of('Hateful 8', 'Django Unchained')
   }));
 });
+
+test('Take two entries to vote', function() {
+  var state = immutable.Map();
+  var entries = ['Hateful 8', 'Django Unchained', 'Pulp Fiction'];
+  var initialState = core.setEntries(state, entries);
+  var nextState = core.next(initialState);
+  expect(nextState).toEqual(
+  immutable.Map({
+    entries: immutable.List.of('Pulp Fiction'),
+    vote: immutable.Map({
+      pair: immutable.List.of('Hateful 8', 'Django Unchained')
+    })
+  }));
+});
--- src/core.js
+++ src/core.js
@@ -6,6 +6,19 @@ function setEntries(state, entries) {
   return state.set('entries', immutable.List(entries));
 }
 
+function next(state) {
+  var entries = state.get('entries');
+  console.log(entries.count());
+  if (entries.count() > 1) {
+    return state.merge({
+      vote: immutable.Map({pair: entries.take(2)}),
+      entries: entries.skip(2)
+    });
+  }
+  return state;
+}
+
 module.exports = {
-  setEntries: setEntries
+  setEntries: setEntries,
+  next: next
 };
