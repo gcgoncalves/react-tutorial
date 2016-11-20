047f53c58daf619ad80475686d610c68498afd55
Gabriel Costa <gcgoncalves88@gmail.com>
2016-11-20 15:59:12 -0200

    Backend complete

--- __tests__/core.js
+++ __tests__/core.js
@@ -1,15 +1,21 @@
 "use strict";
 
 var immutable = require('immutable');
+var matchers = require('jasmine-immutable-matchers');
+
 var core = require('../src/core');
 
 describe('application logic', function() {
+  beforeEach(function () {
+    jest.addMatchers(matchers);
+  });
+
   describe('setEntries', function() {
     it('adds the entries to the state', function () {
       var state = immutable.Map();
       var entries = ['Hateful 8', 'Django Unchained'];
       var nextState = core.setEntries(state, entries);
-      expect(nextState).toEqual(immutable.Map({
+      expect(nextState).toEqualImmutable(immutable.Map({
         entries: immutable.List.of('Hateful 8', 'Django Unchained')
       }));
     });
@@ -21,11 +27,117 @@ describe('application logic', function() {
       var entries = ['Hateful 8', 'Django Unchained', 'Pulp Fiction'];
       var initialState = core.setEntries(state, entries);
       var nextState = core.next(initialState);
-      expect(nextState).toEqual(
+      expect(nextState).toEqualImmutable(
       immutable.Map({
         entries: immutable.List.of('Pulp Fiction'),
         vote: immutable.Map({
           pair: immutable.List.of('Hateful 8', 'Django Unchained')
+        }),
+      }));
+    });
+
+    it('puts winner of current vote back to entries', function() {
+      var state = immutable.Map({
+        entries: immutable.List.of('Pulp Fiction', 'Reservoir Dogs', 'Jackie Brown'),
+        vote: immutable.Map({
+          pair: immutable.List.of('Hateful 8', 'Django Unchained'),
+          tally: immutable.Map({
+            'Hateful 8': 4,
+            'Django Unchained': 2
+          })
+        })
+      });
+      var nextState = core.next(state);
+      expect(nextState).toEqualImmutable(
+        immutable.Map({
+          entries: immutable.List.of('Jackie Brown', 'Hateful 8'),
+          vote: immutable.Map({
+            pair: immutable.List.of('Pulp Fiction', 'Reservoir Dogs'),
+          })
+        })
+      );
+    });
+
+    it('puts both from tied vote back to entries', function() {
+      var state = immutable.Map({
+        entries: immutable.List.of('Pulp Fiction', 'Reservoir Dogs', 'Jackie Brown'),
+        vote: immutable.Map({
+          pair: immutable.List.of('Hateful 8', 'Django Unchained'),
+          tally: immutable.Map({
+            'Hateful 8': 3,
+            'Django Unchained': 3
+          })
+        })
+      });
+      var nextState = core.next(state);
+      expect(nextState).toEqualImmutable(
+        immutable.Map({
+          entries: immutable.List.of('Jackie Brown', 'Hateful 8', 'Django Unchained'),
+          vote: immutable.Map({
+            pair: immutable.List.of('Pulp Fiction', 'Reservoir Dogs'),
+          })
+        })
+      );
+    });
+
+    it('marks winner when just one entry left', function () {
+      var state = immutable.Map({
+        entries: immutable.List.of(),
+        vote: immutable.Map({
+          pair: immutable.List.of('Hateful 8', 'Django Unchained'),
+          tally: immutable.Map({
+            'Hateful 8': 4,
+            'Django Unchained': 2
+          })
+        })
+      });
+      var nextState = core.next(state);
+      expect(nextState).toEqualImmutable(immutable.Map({
+        winner: 'Hateful 8'
+      }));
+    });
+  });
+
+  describe('vote', function() {
+    it('creates a tally for the voted entry', function() {
+      var state = immutable.Map({
+        entries: immutable.List(),
+        vote: immutable.Map({
+          pair: immutable.List.of('Hateful 8', 'Django Unchained')
+        }),
+      });
+      var nextState = core.vote(state, 'Hateful 8');
+      expect(nextState).toEqualImmutable(immutable.Map({
+        entries: immutable.List(),
+        vote: immutable.Map({
+          pair: immutable.List.of('Hateful 8', 'Django Unchained'),
+          tally: immutable.Map({
+            'Hateful 8': 1
+          })
+        }),
+      }));
+    });
+
+    it('adds to existing tally for the voted entry', function() {
+      var state = immutable.Map({
+        entries: immutable.List(),
+        vote: immutable.Map({
+          pair: immutable.List.of('Hateful 8', 'Django Unchained'),
+          tally: immutable.Map({
+            'Hateful 8': 3,
+            'Django Unchained': 2
+          })
+        }),
+      });
+      var nextState = core.vote(state, 'Hateful 8');
+      expect(nextState).toEqualImmutable(immutable.Map({
+        entries: immutable.List(),
+        vote: immutable.Map({
+          pair: immutable.List.of('Hateful 8', 'Django Unchained'),
+          tally: immutable.Map({
+            'Hateful 8': 4,
+            'Django Unchained': 2
+          })
         }),
       }));
     });
--- src/core.js
+++ src/core.js
@@ -7,18 +7,55 @@ function setEntries(state, entries) {
 }
 
 function next(state) {
-  var entries = state.get('entries');
-  console.log(entries.count());
-  if (entries.count() > 1) {
+  var entries = state.get('entries')
+                     .concat(getWinners(state.get('vote')));
+  if (entries.size === 1) {
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first());
+  } else {
     return state.merge({
       vote: immutable.Map({pair: entries.take(2)}),
       entries: entries.skip(2)
     });
   }
-  return state;
+}
+
+function vote(state, entry) {
+  return state.updateIn(
+    ['vote', 'tally', entry],
+    0,
+    function(tally) { return tally + 1; }
+  );
+}
+
+function getWinners(vote) {
+  if (!vote) {
+    return [];
+  }
+
+  var pair = vote.get('pair');
+  pair = _slicedToArray(pair, 2);
+  var a = pair[0];
+  var b = pair[1];
+
+  var aVotes = vote.getIn(['tally', a], 0);
+  var bVotes = vote.getIn(['tally', b], 0);
+
+  if (aVotes > bVotes) { 
+    return [a];
+  } else if  (aVotes < bVotes) { 
+    return [b];
+  } else {
+    return [a, b];
+  }
 }
 
 module.exports = {
   setEntries: setEntries,
-  next: next
+  next: next,
+  vote: vote
 };
+
+
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
