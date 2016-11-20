5fdc0f66963b628cd9c61f7416e38925fb698d2d
Gabriel Costa <gcgoncalves88@gmail.com>
2016-11-20 12:01:52 -0200

    transform tests to describe model

--- __tests__/core.js
+++ __tests__/core.js
@@ -3,25 +3,31 @@
 var immutable = require('immutable');
 var core = require('../src/core');
 
-test('Set entries', function () {
-  var state = immutable.Map();
-  var entries = ['Hateful 8', 'Django Unchained'];
-  var nextState = core.setEntries(state, entries);
-  expect(nextState).toEqual(immutable.Map({
-    entries: immutable.List.of('Hateful 8', 'Django Unchained')
-  }));
-});
+describe('application logic', function() {
+  describe('setEntries', function() {
+    it('adds the entries to the state', function () {
+      var state = immutable.Map();
+      var entries = ['Hateful 8', 'Django Unchained'];
+      var nextState = core.setEntries(state, entries);
+      expect(nextState).toEqual(immutable.Map({
+        entries: immutable.List.of('Hateful 8', 'Django Unchained')
+      }));
+    });
+  });
 
-test('Take two entries to vote', function() {
-  var state = immutable.Map();
-  var entries = ['Hateful 8', 'Django Unchained', 'Pulp Fiction'];
-  var initialState = core.setEntries(state, entries);
-  var nextState = core.next(initialState);
-  expect(nextState).toEqual(
-  immutable.Map({
-    entries: immutable.List.of('Pulp Fiction'),
-    vote: immutable.Map({
-      pair: immutable.List.of('Hateful 8', 'Django Unchained')
-    })
-  }));
+  describe('next', function() {
+    it('takes the next two entries under vote', function() {
+      var state = immutable.Map();
+      var entries = ['Hateful 8', 'Django Unchained', 'Pulp Fiction'];
+      var initialState = core.setEntries(state, entries);
+      var nextState = core.next(initialState);
+      expect(nextState).toEqual(
+      immutable.Map({
+        entries: immutable.List.of('Pulp Fiction'),
+        vote: immutable.Map({
+          pair: immutable.List.of('Hateful 8', 'Django Unchained')
+        }),
+      }));
+    });
+  });
 });
